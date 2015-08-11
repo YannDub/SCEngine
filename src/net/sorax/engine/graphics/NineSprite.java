d8575162037ec7d8e4b40b3b577c0a5871fc5c81
S0orax <ttreplay@gmail.com>
2015-08-11 18:25:48 +0200

    [NineSprite] Add position to the constructor

--- src/net/sorax/engine/graphics/NineSprite.java
+++ src/net/sorax/engine/graphics/NineSprite.java
@@ -24,7 +24,7 @@ public class NineSprite extends Image {
 		}
 	}
 	
-	public NineSprite(Sprite sprite, float nineWidth, float nineHeight) {
+	public NineSprite(Sprite sprite, float x, float y, float nineWidth, float nineHeight) {
 		super(sprite.getTexture(), sprite.getSpriteWidth(), sprite.getSpriteHeight());
 		this.sprites = new Sprite[9];
 		int spriteWidth = (int) (sprite.getSpriteWidth()) / 3;
