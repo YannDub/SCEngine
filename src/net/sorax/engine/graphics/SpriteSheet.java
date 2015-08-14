dc50e6f3d7aa55692644b36464694a5ed83ba883
S0orax <ttreplay@gmail.com>
2015-08-14 11:33:19 +0200

    SpriteSheet integration

---
+++ src/net/sorax/engine/graphics/SpriteSheet.java
@@ -0,0 +1,67 @@
+package net.sorax.engine.graphics;
+
+public class SpriteSheet {
+	
+	private Texture texture;
+	private float textureWidth, textureHeight, spriteWidth, spriteHeight, renderWidth, renderHeight;
+	private Sprite[] sprites;
+	
+	public SpriteSheet(Texture texture, float spriteWidth, float spriteHeight, float renderWidth, float renderHeight) {
+		this.texture = texture;
+		this.textureWidth = texture.getWidth();
+		this.textureHeight = texture.getHeight();
+		this.spriteWidth = spriteWidth;
+		this.spriteHeight = spriteHeight;
+		this.renderWidth = renderWidth;
+		this.renderHeight = renderHeight;
+		
+		int nbSpriteWidth = (int) (textureWidth / spriteWidth);
+		int nbSpriteHeight = (int) (textureHeight / spriteHeight);
+		
+		this.sprites = new Sprite[nbSpriteWidth * nbSpriteHeight];
+		
+		for(int j = 0; j < nbSpriteHeight; j++) {
+			for(int i = 0; i < nbSpriteWidth; i++) {
+				int frame = i + j * nbSpriteWidth;
+				this.sprites[frame] = new Sprite(texture, 0, 0, renderWidth, renderHeight, spriteWidth, spriteHeight, frame);
+			}
+		}
+		
+	}
+	
+	public SpriteSheet(String texture, float spriteWidth, float spriteHeight, float renderWidth, float renderHeight) {
+		this(Texture.loadTexture(texture), spriteWidth, spriteHeight, renderWidth, renderHeight);
+	}
+	
+	public Texture getTexture() {
+		return this.texture;
+	}
+	
+	public float getWidth() {
+		return this.textureWidth;
+	}
+	
+	public float getHeight() {
+		return this.getHeight();
+	}
+	
+	public float getSpriteWidth() {
+		return this.spriteWidth;
+	}
+	
+	public float getSpriteHeight() {
+		return this.spriteHeight;
+	}
+	
+	public float getRenderWidth() {
+		return this.renderWidth;
+	}
+	
+	public float getRenderHeight() {
+		return this.renderHeight;
+	}
+	
+	public Sprite getSprite(int frame) {
+		return this.sprites[frame];
+	}
+}
